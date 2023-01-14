90d71c4833c44b99144c1a83fc5be5a370a65f82
Jake Bowhay <jb9.bowhay@gmail.com>
2023-01-14 16:12:39 +0000

    mid commit

--- preliminary_investigations/optimisation.py
+++ preliminary_investigations/optimisation.py
@@ -3,29 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import dccp
 
-n = 20
-r = [1 for i in range(n)]
+n = 2
 
+d = Variable()
 c = Variable(shape=(n,2))
 constr = []
 for i in range(n-1):
     for j in range(i+1,n):
-        constr.append(norm(c[i,:]-c[j,:])>=r[i]+r[j])
-prob = Problem(Minimize(max(max(abs(c),axis=1)+r)), constr)
-prob.solve(method = 'dccp', ccp_times = 1)
+        constr.append(norm(c[i,:]-c[j,:])>=d)
 
-l = max(max(abs(c),axis=1)+r).value*2
-pi = np.pi
-ratio = pi*sum(square(r)).value/square(l).value
-print("ratio =", ratio)
-print(prob.status)
+prob = Problem(Maximize(d), constr + [0 <= c, c<=1 ])
 
-# plot
-fig, ax = plt.subplots()
-circ = np.linspace(0,2*pi)
-for i in range(n):
-    ax.plot(c[i,0].value+r[i]*np.cos(circ),c[i,1].value+r[i]*np.sin(circ),'b')
-ax.set_aspect('equal')
-ax.set_xlim([-l/2,l/2])
-ax.set_ylim([-l/2,l/2])
-plt.show()
+print(dccp.is_dccp(prob))
+
+prob.solve(method='dccp',ccp_times=10,max_iter=1000)
--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 
 import scipy
 import numpy as np
+import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
@@ -12,28 +13,76 @@ class PackingMethod(ABC):
     """
 
     def __init__(self) -> None:
-        self.n: int = None
-        self.xi: np.ndarray = None
-        self.ri: np.ndarray = None
+        self.n: int | None = None
+        self.xi: np.ndarray | None = None
+        self.ri: np.ndarray | None = None
+        self.source_nodes: np.ndarray | None = None
+        self.sink_nodes: np.ndarray | None = None
 
     @abstractmethod
     def generate_packing(self, r: float) -> None:
         ...
 
-    def plot_packing(self) -> None:
-        fig, ax = plt.subplots()
+    def _add_node_patches(self, ax: plt.Axes, colors: np.ndarray | None = None) -> None:
+        """Adds circle patches to an axes at the location of each of the particles.
 
-        for r, point in zip(self.ri, self.xi):
-            ax.add_patch(patches.Circle(point, r))
-
+        Parameters
+        ----------
+        ax : plt.Axes
+            The axes to add the patches too.
+        colors : np.ndarray | None, optional
+            Array of colors for each of the patches, by default None
+        """
+        if colors is None:
+            colors = np.repeat("tab:blue", self.ri.size)
+        for r, point, color in zip(self.ri, self.xi, colors):
+            ax.add_patch(patches.Circle(point, r, color=color))
         ax.set_xlim((0, 1))
         ax.set_ylim((0, 1))
         ax.set_aspect("equal")
+    
+    def _check_packing_created(self) -> None:
+        if self.xi is None:
+            raise ValueError("Need to generate packing first.")
+
+    def plot_packing(self) -> None:
+        """Plot the basic packing."""
+        self._check_packing_created
+        fig, ax = plt.subplots()
+        self._add_node_patches(ax)
+        plt.show()
+
+    def generate_network(self) -> None:
+        """Create the network representation of the packing."""
+        self._check_packing_created()
+        
+        # find which nodes are sources and which are sinks
+        self.source_nodes = np.argwhere(np.abs(self.xi[:, 1] - 1) <= self.ri).squeeze()
+        self.sink_nodes = np.argwhere(np.abs(self.xi[:, 1]) <= self.ri).squeeze()
+        
+        adjacency = np.zeros((self.n, self.n))
+        tree = scipy.spatial.KDTree(self.xi)
+
+    def plot_network(self) -> None:
+        """Plot network representation of the packing."""
+        fig, ax = plt.subplots()
+        colors = np.repeat("tab:blue", self.n).astype("object")
+        colors[self.sink_nodes] = "tab:red"
+        colors[self.source_nodes] = "tab:green"
+        self._add_node_patches(ax, colors)
+        ax.plot(self.xi[:, 0], self.xi[:, 1], "k.", markersize=10)
         plt.show()
 
 
 class RegularPacking(PackingMethod):
     def generate_packing(self, r: float) -> None:
+        """Generates a regular packed grid of circles
+
+        Parameters
+        ----------
+        r : float
+            radius of circles
+        """
         self.n = int(1 / (2 * r)) ** 2
 
         tmp = np.arange(r, 1, 2 * r)
@@ -45,3 +94,5 @@ if __name__ == "__main__":
     p = RegularPacking()
     p.generate_packing(0.05)
     p.plot_packing()
+    p.generate_network()
+    p.plot_network()
